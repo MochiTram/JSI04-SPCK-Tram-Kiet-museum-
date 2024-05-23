11fd84eb2258d5e07cea69d2f7d995c5fb7ab78a
Mochitram <misslacquan@gmail.com>
2024-05-23 19:47:48 +0700

    trang chu và data

--- main.js
+++ main.js
@@ -32,5 +32,5 @@ function carousel() {
     myIndex++;
     if (myIndex > x.length) { myIndex = 1 }
     x[myIndex - 1].style.display = "block";
-    setTimeout(carousel, 10000); // Change image every 10 seconds
+    setTimeout(carousel, 5000); // Change image every 5 seconds
 }
