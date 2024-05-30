a5bceb846a90fe8dc75f0984ab8a4cfed0bf6448
tuankietcao <caotuankiet2008@gmail.com>
2024-05-30 14:03:14 +0700

    chot

--- main.js
+++ main.js
@@ -36,3 +36,20 @@ function carousel() {
 }
 
 
+// async function getData() {
+//     const res = await fetch('./data.json')
+//     // The return value is *not* serialized
+//     // You can return Date, Map, Set, etc.
+   
+//     if (!res.ok) {
+//       // This will activate the closest `error.js` Error Boundary
+//       throw new Error('Failed to fetch data')
+//     }
+   
+//     return res.json()
+//   }
+   
+// export default async function Page() {
+//     const data = await getData()}
+
+// console.log(data)
--- signup.js
+++ signup.js
@@ -82,3 +82,5 @@ function validateField(field) {
         return true
     }
 }
+
+
